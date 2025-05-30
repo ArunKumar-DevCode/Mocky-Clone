2292771e62672ad95ddcf41fd7042a651a57517d
Arunkumar-DevCode <arunkum@guvi.in>
2025-05-30 14:08:29 +0530

    bug fixed : axios update base url

--- Client/lib/axios.ts
+++ Client/lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 const baseURL =
   process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "https://mock-clone-vx69.onrender.com";
+  "https://mock-clone-vx69.onrender.com/api";
 
 const apiClient: AxiosInstance = axios.create({
   baseURL,
