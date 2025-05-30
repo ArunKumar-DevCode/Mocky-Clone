62613b7f7471f88f7e51cf0e8a3a863bf8632e80
Arunkumar-DevCode <arunkum@guvi.in>
2025-05-30 09:23:42 +0530

    bug fixed

--- Client/app/design/page.tsx
+++ Client/app/design/page.tsx
@@ -1,16 +1,14 @@
 import NewMockForm from "@/components/NewMockForm";
-import { getCookie } from 'cookies-next/server';
 
 export default async function NewMock() {
-  const accessToken = await getCookie("accessToken");
-
+  
   return (
     <section>
       <h1 className="text-center text-4xl font-light my-6 p-5">
         Design New Mock
       </h1>
       <div className="flex justify-center items-center w-full py-12 bg-gradient-to-br from-slate-50 to-blue-50">
-        <NewMockForm accessToken={accessToken} />
+        <NewMockForm/>
       </div>
     </section>
   );
