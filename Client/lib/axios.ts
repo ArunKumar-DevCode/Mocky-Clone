3b60240ef08ccf704fae30f31eb519c81a369105
Arunkumar-DevCode <arunkum@guvi.in>
2025-05-30 14:26:29 +0530

    Modified: axios config update

--- Client/lib/axios.ts
+++ Client/lib/axios.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "https://mock-clone-vx69.onrender.com/api";
+const baseURL = "https://mock-clone-vx69.onrender.com/api";
 
 const apiClient: AxiosInstance = axios.create({
   baseURL,
