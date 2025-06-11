a24054cf2345da3d9a80f4062180627c726272a2
Arunkumar-DevCode <arunkum@guvi.in>
2025-06-11 17:41:02 +0530

    Update video player UI

--- Client/components/Testimonial.tsx
+++ Client/components/Testimonial.tsx
@@ -8,7 +8,6 @@ import { useState } from "react";
 export default function Testimonial() {
   const [isVideoLoaded, setIsVideoLoaded] = useState(false);
   const [showVideo, setShowVideo] = useState(false);
-  console.log(isVideoLoaded);
   return (
     <>
       <section className="relative w-full min-h-screen py-18 flex justify-center items-center">
@@ -71,9 +70,8 @@ export default function Testimonial() {
                       className="w-full h-full object-cover"
                       onLoadedData={() => setIsVideoLoaded(true)}
                       title="Mock API Creation Demo"
-                      autoPlay
+                      autoPlay={isVideoLoaded}
                       controls
-                      muted
                     />
                   )}
                 </div>
