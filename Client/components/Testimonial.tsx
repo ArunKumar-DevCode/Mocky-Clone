17a9a83b94cfc127b260fa34cab42f3090ec2563
Arunkumar-DevCode <arunkum@guvi.in>
2025-06-11 17:24:26 +0530

    Video ui updated

--- Client/components/Testimonial.tsx
+++ Client/components/Testimonial.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 export default function Testimonial() {
   const [isVideoLoaded, setIsVideoLoaded] = useState(false);
   const [showVideo, setShowVideo] = useState(false);
-
+  console.log(isVideoLoaded);
   return (
     <>
       <section className="relative w-full min-h-screen py-18 flex justify-center items-center">
